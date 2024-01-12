ada52ae93b5e65a5eb666f326077ca0e5104c103
thanhngodev <thanhngodev@gmail.com>
2024-01-12 22:58:44 +0700

    handle signin and signup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 dotenv.config();
 
 mongoose
@@ -14,6 +17,21 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, (err, res) => {
     console.log("listening with port 3000");
+})
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
 })
