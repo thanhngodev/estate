b374acfcdfc85db75b92954c2bca2928c705cc78
thanhngodev <thanhngodev@gmail.com>
2024-01-23 00:19:50 +0700

    setup build

--- api/index.js
+++ api/index.js
@@ -18,7 +18,6 @@ mongoose
     });
 
 const __dirname = path.resolve();
-
 const app = express();
 
 app.use(express.json());
@@ -34,9 +33,10 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
+
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist' ,'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
