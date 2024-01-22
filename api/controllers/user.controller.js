14fe02faa08fcebc3851b00f3f7ea7e32821cca3
thanhngodev <thanhngodev@gmail.com>
2024-01-22 23:23:14 +0700

    handle comppleted page listings

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -32,7 +32,7 @@ export const updateUser = async (req, res, next) => {
 }
 
 export const deleteUser = async (req, res, next) => {
-    if(req.user.id != req.user.id) return next(errorHandler(401, 'You can only delete your own account!'));
+    if (req.user.id != req.user.id) return next(errorHandler(401, 'You can only delete your own account!'));
     try {
         await User.findByIdAndDelete(req.params.id);
         res.clearCookie('access_token');
@@ -43,7 +43,7 @@ export const deleteUser = async (req, res, next) => {
 }
 
 export const getUserListings = async (req, res, next) => {
-    if( req.user.id === req.params.id ) {
+    if (req.user.id === req.params.id) {
         try {
             const listings = await Listing.find({ userRef: req.params.id });
             res.status(200).json(listings);
@@ -56,5 +56,12 @@ export const getUserListings = async (req, res, next) => {
 }
 
 export const getUser = async (req, res, next) => {
-
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) return next(errorHandler(404, 'User not found!'));
+        const { password: pass, ...rest } = user._doc;
+        res.status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 }
